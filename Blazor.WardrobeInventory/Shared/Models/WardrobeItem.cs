2b9c92a93b476408af8b4ac610021a416f985b0b
MarianoDesivo <mariano_desivo@hotmail.com>
2023-03-15 12:29:02 -0300

    Add project files.

---
+++ Blazor.WardrobeInventory/Shared/Models/WardrobeItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Blazor.WardrobeInventory.Shared.Models;
+
+public class WardrobeItem
+{
+    public int Id { get; set; }
+    [Required]
+    [Display(Name = "Shirt/Jeans/Jacket/...")]
+
+    public string Type { get; set; }
+    [Display(Name = "Item Color")]
+    public string Color { get; set; }
+    public string ImagePath { get; set; }
+}
